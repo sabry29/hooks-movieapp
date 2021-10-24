88d0edeefa1413bebea21ba6e475860f54f0cd30
sabry <touihrimedsabri@gmail.com>
2021-10-24 12:11:24 +0100

    first

--- src/App.js
+++ src/App.js
@@ -1,24 +1,39 @@
-import logo from './logo.svg';
 import './App.css';
+import {moviesList} from './assets/mookData';
+import MoviesList from './component/MoviesList';
+import AddMovie from './component/addMovie';
+import SearchBar from './component/SearchBar';
+
+import React, { useState } from 'react';
 
 function App() {
+
+  const [movieList, setMovieList] = useState(moviesList);
+  const addMovie =(newMovie) => setMovieList([...moviesList,newMovie]);
+  const [searchWord, setSearchWord] = useState('');
+  const handleSearch = (e) => setSearchWord(e.target.value);
+  const [ratingSearch, setRatingSearch] = useState(0);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div className='container'>
+      <SearchBar
+        handleSearch={handleSearch}
+        setRatingSearch={setRatingSearch}
+        ratingSearch={ratingSearch}
+      />
+     <MoviesList
+        moviesArray={
+          searchWord
+            ? movieList.filter((movie) =>
+                movie.title.toLowerCase().includes(searchWord.toLowerCase())
+              )
+            : ratingSearch > 1
+            ? movieList.filter((movie) => movie.rate >= ratingSearch)
+            : movieList
+        }
+      />
+      <AddMovie handleAdd={addMovie} />
+  </div>
   );
 }
 
---
+++ src/component/MoviesList.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import PropTypes  from 'prop-types';
+import MovieCard from './MovieCard';
+
+const MoviesList= ({moviesArray}) => {
+    return (
+        <div className='row mt-2'>
+            {moviesArray.map((movie,key)=> (
+            <MovieCard movie={movie} key={key}/>
+        ))}
+       </div>
+    );
+    };
+    MoviesList.propTypes = {
+    moviesArray: PropTypes.array.isRequired,
+    };
+export default MoviesList;
